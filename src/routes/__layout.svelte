0cd6bd5096afc52b529820cdc4250807bf8a00d3
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-24 02:21:31 +0530

    re-init entire app

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import '../app.css';
-	import '../app.postcss';
 </script>
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
