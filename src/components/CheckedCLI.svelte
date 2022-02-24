d412c2e7a94e09d45d4cb9b46d1143a609b34b74
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-24 16:07:22 +0530

    added 'select all' button and improved layout

--- src/components/CheckedCLI.svelte
+++ src/components/CheckedCLI.svelte
@@ -5,6 +5,7 @@
 	export let horizontal: string = 'w-4/5 md:w-3/5 mx-auto';
 	export let vSpacing: string = 'mt-12';
 	export let checkedCLI: string = '~~FIXME: checkedCLI not passed';
+	export let isSelectAll: boolean = false;
 
 	// Copy to clipboard
 	function copy(cliCommand: string) {
@@ -19,11 +20,16 @@
 
 <div class="{horizontal} {vSpacing} w-full mb-12">
 	<div class="px-8 py-4 bg-neutral-900 rounded-lg shadow-md">
-		<div class="prose prose-invert mt-2 max-w-none">
-			<h2 id="select-apps-you-want-">Personalized List 🌠</h2>
+		<div class="flex place-content-between">
+			<h2 class="text-2xl font-bold">Personalized List 🌠</h2>
+			<div class="flex items-center mt-2">
+				<input class="w-4 h-4 accent-violet-400" type="checkbox" bind:checked={isSelectAll} />
+				<p class="ml-2">Select all apps</p>
+			</div>
 		</div>
+
 		<!-- Winget CLI -->
-		<div class="flex px-8 py-8 space-x-4 items-end align-text-bottom">
+		<div class="flex py-8 space-x-4 items-end align-text-bottom">
 			<textarea
 				type="text"
 				class="h-12 overflow-x-auto pt-3 w-full text-violet-200 text-center font-mono bg-transparent rounded ring-2 ring-violet-400"
@@ -39,13 +45,6 @@
 		</div>
 	</div>
 </div>
+
 <!-- Hidden element to enable copy to clipboard -->
 <div id="clipboard" />
-
-<style>
-	/* Checkbox */
-	.checkmark {
-		height: 25px;
-		width: 25px;
-	}
-</style>
