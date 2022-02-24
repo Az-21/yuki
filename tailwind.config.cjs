2863789bc5817896f28688ecf6b57fce5df51076
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-24 11:22:53 +0530

    added tailwind typography module

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const typography = require('@tailwindcss/typography');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
@@ -5,7 +7,7 @@ const config = {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [typography]
 };
 
 module.exports = config;
