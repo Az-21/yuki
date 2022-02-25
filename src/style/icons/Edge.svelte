4fb132be267436ca9a3901783261758fd281f423
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-26 01:09:31 +0530

    removed left padding (1st element)

--- src/style/icons/Edge.svelte
+++ src/style/icons/Edge.svelte
@@ -2,7 +2,7 @@
   export let id: string;
 
   const url: string = 'https://microsoftedge.microsoft.com/addons/detail/';
-  const icon: string = 'fa-brands fa-edge fa-xl text-emerald-400 pl-4';
+  const icon: string = 'fa-brands fa-edge fa-xl text-emerald-400';
 </script>
 
 <a href="{url}{id}"><i class={icon} /></a>
