13b786da2aa09ebe6bf9b49cbe2955b51e4df887
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-26 00:31:10 +0530

    new utility icons

---
+++ src/style/icons/Globe.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let url: string;
+
+  const icon: string = 'fa-solid fa-globe fa-xl text-white pl-4';
+</script>
+
+<a href={url}><i class={icon} /></a>
---
+++ src/style/icons/RightArrow.svelte
@@ -0,0 +1,1 @@
+<i class="fa-solid fa-angles-right mr-2 text-violet-400" />
