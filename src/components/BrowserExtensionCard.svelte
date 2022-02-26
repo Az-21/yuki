c16e8edd4ff229d7f61555366acd9316792afc8d
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-26 11:39:12 +0530

    extension card component

---
+++ src/components/BrowserExtensionCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Chrome from '$icons/Chrome.svelte';
+  import Edge from '$icons/Edge.svelte';
+  import Firefox from '$icons/Firefox.svelte';
+  import GitHub from '$icons/GitHub.svelte';
+  import Globe from '$icons/Globe.svelte';
+  import RightArrow from '$icons/RightArrow.svelte';
+
+  export let name: string;
+  export let info: string;
+  export let chromeId: string;
+  export let edgeId: string;
+  export let firefoxId: string;
+  export let website: string;
+
+  const prose: string = 'prose prose-invert max-w-lg';
+</script>
+
+<div class={prose}>
+  <h3><RightArrow /> {name}</h3>
+  <ul>
+    <li>
+      <Edge id={edgeId} />
+      <Chrome id={chromeId} />
+      <Firefox id={firefoxId} />
+      {#if website.slice(0, 5) === 'http'}
+        <Globe url={website} />
+      {:else}
+        <GitHub id={website} />
+      {/if}
+    </li>
+    <li>{info}</li>
+  </ul>
+</div>
