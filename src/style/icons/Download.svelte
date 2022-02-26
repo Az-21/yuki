14ff03189823761c0bc5bcad44ae2c63ff8ad509
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-27 02:20:21 +0530

    changed download button color to white

--- src/style/icons/Download.svelte
+++ src/style/icons/Download.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let url: string;
-  const icon: string = 'fa-solid fa-download fa-xl text-emerald-500';
+  const icon: string = 'fa-solid fa-download fa-xl text-white';
 </script>
 
 <a href={url}><i class={icon} /></a>
