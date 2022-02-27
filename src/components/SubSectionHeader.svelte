466a0cc52eaff9fc0410a21eef2331fdff888162
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-02-27 11:25:05 +0530

    added subsection header component

---
+++ src/components/SubSectionHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let text: string;
+  export let icon: string;
+  export let spacing: string;
+
+  icon += ' fa-xl';
+</script>
+
+<div class={spacing}>
+  <div class="prose prose-invert flex max-w-none items-baseline gap-6">
+    <i class={icon} />
+    <h2> {text} </h2>
+  </div>
+  <hr class="border-b-1 border-white opacity-40" />
+</div>
