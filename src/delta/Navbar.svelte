ab8567febbf99c09ebaccbf4a81626f0b265a2dd
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-09-02 23:31:00 +0530

    🩹 fix navbar padding

--- src/delta/Navbar.svelte
+++ src/delta/Navbar.svelte
@@ -1,9 +1,11 @@
-<nav class="flex items-end justify-between bg-neutral-900 py-5 px-12 text-white shadow-lg">
-  <div class="pl-2 text-3xl font-medium">Y U K I</div>
-  <div class="font-medium">
-    <a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki/blob/main/README.md">
-      About
-    </a>
-    <a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki"> GitHub </a>
-  </div>
+<nav class=" bg-neutral-900 p-5 mb-12 text-white">
+	<div class="w-11/12 mx-auto flex items-end justify-between">
+		<div class="text-3xl font-medium">Y U K I</div>
+		<div class="font-medium">
+			<a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki/blob/main/README.md">
+				About
+			</a>
+			<a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki"> GitHub </a>
+		</div>
+	</div>
 </nav>
