415b9b078e41136f5723a8bfeca905538241eb97
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-07-08 21:59:36 +0530

    Add star repo icon to navbar

--- src/delta/Navbar.svelte
+++ src/delta/Navbar.svelte
@@ -4,6 +4,9 @@
     <div class="font-medium">
       <a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki/blob/main/README.md"> About </a>
       <a class="px-2 hover:text-green-300" href="https://github.com/Az-21/yuki"> GitHub </a>
+      <a class="px-2 hover:text-emerald-300" href="https://github.com/Az-21/yuki/stargazers">
+        <i class="fa-solid fa-star text-emerald-500 hover:scale-150 transition" />
+      </a>
     </div>
   </div>
 </nav>
