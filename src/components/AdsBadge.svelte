4aab77d1ef1565f1176d5ff581b2a5722eed9b3a
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-01-19 15:08:50 +0530

    Improve monetization/ads badge

--- src/components/AdsBadge.svelte
+++ src/components/AdsBadge.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
   export let revenue: number;
+  const button: string = 'h-max rounded bg-neutral-800 p-2 text-xs font-black uppercase text-neutral-200';
 </script>
 
 {#if revenue === 0}
-  <button disabled class="h-max rounded bg-green-500 p-2 text-xs font-black uppercase text-neutral-900">
-    <i class="fa-solid fa-heart" /> Ad Free
+  <button disabled class={button}>
+    <i class="fa-solid fa-heart text-green-500 pr-1" /> Ad Free
   </button>
 {:else if revenue === 1}
-  <button disabled class="h-max rounded bg-indigo-500 p-2 text-xs font-black uppercase text-neutral-900">
-    <i class="fa-solid fa-rectangle-ad" /> Ad Supported
+  <button disabled class={button}>
+    <i class="fa-solid fa-rectangle-ad text-indigo-500 pr-1" /> Ad Supported
   </button>
 {:else}
-  <button disabled class="h-max rounded bg-red-500 p-2 text-xs font-black uppercase text-neutral-900">
-    <i class="fa-solid fa-money-check-dollar" /> Paid App
+  <button disabled class={button}>
+    <i class="fa-solid fa-money-check-dollar text-red-500 pr-1" /> Paid App
   </button>
 {/if}
