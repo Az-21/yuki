4566c86e6368088643be02afc315dc15e8a9137c
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-01-19 14:50:55 +0530

    Improve Android app card style

--- src/components/PlayStoreAndroidApp.svelte
+++ src/components/PlayStoreAndroidApp.svelte
@@ -1,25 +1,24 @@
 <script lang="ts">
   import AdsBadge from '$components/AdsBadge.svelte';
+  import RightArrow from '$icons/RightArrow.svelte';
 
   export let title: string;
   export let info: string;
   export let playStoreId: string;
   export let revenue: number;
-  const prose: string = 'prose prose-invert';
 
   let url: string = `https://play.google.com/store/apps/details?id=${playStoreId}`;
 </script>
 
-<div class="bg-neutral-900 p-6 rounded-lg w-full">
-  <div class={prose}>
-    <h3 class="flex flex-row items-end place-content-between">
-      <a href={url} class="flex flex-row no-underline hover:text-emerald-400">
-        {title}
-        <i class="pl-2 scale-75 fa-solid fa-sm fa-up-right-from-square text-white" />
-      </a>
-      <AdsBadge {revenue} />
-    </h3>
-
-    <div class="pt-4">{info}</div>
+<div class="bg-neutral-900 p-6 rounded-lg w-full text-white">
+  <div class="flex pt-4 flex-row items-baseline justify-between">
+    <a href={url} class="flex flex-row items-baseline no-underline hover:text-emerald-400">
+      <RightArrow />
+      <p class="text-xl font-bold">{title}</p>
+      <i class="pl-2 scale-75 fa-solid fa-up-right-from-square text-white" />
+    </a>
+    <AdsBadge {revenue} />
   </div>
+
+  <div class="pt-8">{info}</div>
 </div>
