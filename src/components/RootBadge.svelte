859af79ae76fbcf95be1754c873b09570e62b97e
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-01-19 15:04:53 +0530

    Improve root badge style

--- src/components/RootBadge.svelte
+++ src/components/RootBadge.svelte
@@ -2,15 +2,12 @@
   export let rootRequired: number;
 </script>
 
-{#if rootRequired === 0}
-  <button disabled class="h-max rounded bg-green-500 p-2 text-xs font-black uppercase text-neutral-900">
-    Root <i class="fa-solid fa-caret-right px-1" /> Not Required
+{#if rootRequired === 1}
+  <button disabled class="h-max rounded bg-neutral-800 p-2 text-xs font-black uppercase text-neutral-200">
+    More Features with Root
   </button>
-{:else if rootRequired === 1}
-  <button disabled class="h-max rounded bg-indigo-500 p-2 text-xs font-black uppercase text-neutral-900">
-    Root <i class="fa-solid fa-caret-right px-1" /> Optional
-  </button>
-{:else}
+{/if}
+{#if rootRequired === 2}
   <button disabled class="h-max rounded bg-red-500 p-2 text-xs font-black uppercase text-neutral-900">
     Root <i class="fa-solid fa-caret-right px-1" /> Required
   </button>
